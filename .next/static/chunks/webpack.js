03d3e0d55ae97f29064fe6e4dc4ffce38cb7b6b1
saif <saif8502@gmail.com>
2023-03-30 23:53:26 -0500

    Added Media queries to arrows

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "423114e9c195c8c9"; }
+/******/ 		__webpack_require__.h = function() { return "e2edcd30259f396b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
