49c42b9e6d5b247db0d9faa45cc5b9f7d5b0bfc5
saif <saif8502@gmail.com>
2023-03-31 00:12:49 -0500

    Media queries for nav

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e2edcd30259f396b"; }
+/******/ 		__webpack_require__.h = function() { return "0d85d89c2387684b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
