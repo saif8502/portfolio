a027a8ab0c0849844b266b68cede1cb2f2001f97
saif <saif8502@gmail.com>
2023-04-01 21:40:26 -0500

    Changed Title

--- pages/slider.js
+++ pages/slider.js
@@ -3,7 +3,6 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-
 function PortfolioSlider() {
   const settings = {
     dots: false,
@@ -20,27 +19,29 @@ function PortfolioSlider() {
           slidesToShow: 1,
           slidesToScroll: 1,
           infinite: true,
-          dots: true
-        }
+          dots: true,
+        },
       },
       {
         breakpoint: 600,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
-          initialSlide: 1
-        }
-      }
-    ]
+          initialSlide: 1,
+        },
+      },
+    ],
   };
 
   function NextArrow(props) {
-    
     const { onClick } = props;
     return (
-      <div className="arrow next" onClick={onClick} style={{ fontSize: "40px" }}>
+      <div
+        className="arrow next"
+        onClick={onClick}
+        style={{ fontSize: "40px" }}
+      >
         &#10148;
-           
       </div>
     );
   }
@@ -48,40 +49,51 @@ function PortfolioSlider() {
   function PrevArrow(props) {
     const { onClick } = props;
     return (
-      <div className="arrow prev" onClick={onClick} style={{ fontSize: "40px",transform: 'rotate(180deg)', transformOrigin: '50% 41%',display: 'inline-block' }}>
-       &#10148;
+      <div
+        className="arrow prev"
+        onClick={onClick}
+        style={{
+          fontSize: "40px",
+          transform: "rotate(180deg)",
+          transformOrigin: "50% 41%",
+          display: "inline-block",
+        }}
+      >
+        &#10148;
       </div>
     );
   }
 
   return (
     <div className="slider">
-    <div className="portfolio-slider">
-      <Slider {...settings}>
+      <div className="portfolio-slider">
+        <Slider {...settings}>
+          <div className="portfolio-item">
+            <h3 className="text-md py-5 leading-8 text-gray-800 dark:text-gray-200 max-w-xl mx-auto md:text-xl">
+              Hello Friends!
+              <br></br>
+              My name is Muhammed Saif, I am a Full-Stack Developer with a
+              strong passion of working on React Apps.
+            </h3>
+          </div>
 
-        <div className="portfolio-item">
-       
-          <h3 className="text-md py-5 leading-8 text-gray-800 dark:text-gray-200 max-w-xl mx-auto md:text-xl">Hello Friends! 
-          <br>
-          </br>
-          My name is Muhammed Saif, I am a Full-Stack Developer with a strong passion of working on React Apps.</h3>
-        </div>
+          <div className="portfolio-item">
+            <br></br>
+            <h3 className="text-md py-5 leading-8 text-gray-800 dark:text-gray-200 max-w-xl mx-auto md:text-xl">
+              In 2022, I made a decision to change careers and joined Trine
+              University for my Masters in Information Systems
+            </h3>
+          </div>
 
-        <div className="portfolio-item">
-          
-          <br></br>
-          <h3 className="text-md py-5 leading-8 text-gray-800 dark:text-gray-200 max-w-xl mx-auto md:text-xl">In 2022, I made a decision to change careers and joined Trine University for my Masters in Information Systems</h3>
-        </div>
-        
-        <div className="portfolio-item">
-        
-          <br></br>
-          <h3 className="text-md py-5 leading-8 text-gray-800 dark:text-gray-200 max-w-xl mx-auto md:text-xl">When I am not coding, I spend most of the time playing video games.</h3>
-          
-        </div>
-       
-      </Slider>
-    </div>
+          <div className="portfolio-item">
+            <br></br>
+            <h3 className="text-md py-5 leading-8 text-gray-800 dark:text-gray-200 max-w-xl mx-auto md:text-xl">
+              When I am not coding, I spend most of the time playing video
+              games.
+            </h3>
+          </div>
+        </Slider>
+      </div>
     </div>
   );
 }
